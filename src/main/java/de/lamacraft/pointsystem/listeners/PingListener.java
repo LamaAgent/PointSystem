ee314e976f3bd5b0bf08e097005dc222de196027
LamaAgent <78685728+LamaAgent@users.noreply.github.com>
2022-04-16 00:04:06 +0200

    Changed PingListener

--- src/main/java/de/lamacraft/pointsystem/listeners/PingListener.java
+++ src/main/java/de/lamacraft/pointsystem/listeners/PingListener.java
@@ -1,5 +1,6 @@
 package de.lamacraft.pointsystem.listeners;
 
+import de.lamacraft.pointsystem.utils.FileManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
@@ -8,9 +9,14 @@ public class PingListener implements Listener {
 
     @EventHandler
     public void onPing(ServerListPingEvent e) {
-        e.setMaxPlayers(10);
-        e.setMotd("\u0020\u0020\u0020\u0020\u0020§dViel Spaß beim K\u00e4mpfen um die meisten Punkte!\n" +
-                "\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020§eMöge der oder die Beste gewinnen!");
+        if (!FileManager.getConfigFileConfiguration().getBoolean("maintance")) {
+            e.setMaxPlayers(10);
+            e.setMotd("\u0020\u0020\u0020\u0020\u0020§dViel Spaß beim K\u00e4mpfen um die meisten Punkte!\n" +
+                    "\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020§eMöge der oder die Beste gewinnen!");
+        } else {
+            e.setMaxPlayers(0);
+            e.setMotd("§4Du kannst dem Server zurzeit nicht joinen!");
+        }
     }
 
 }
